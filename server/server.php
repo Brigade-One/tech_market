bb794429807761830050bfd8b69e558c096da749
Vitalii Petrun <vitalii.petrun@gmail.com>
2023-04-30 09:27:32 +0300

    added server G P P D handlers, client can send these requests

--- server/server.php
+++ server/server.php
@@ -1,13 +1,30 @@
 <?php
+
 $host = 'techmarket';
 $port = 80;
 $docroot = __DIR__;
 
+// Start server
 $cmd = sprintf('php -S %s:%d -t %s', $host, $port, $docroot);
 shell_exec($cmd);
 
 // Handle the request
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    echo 'Hello, world!';
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === 'GET') {
+    echo '[GET] Hello, world!';
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo '[POST] Data received';
+} elseif ($method === 'PUT') {
+    $data = file_get_contents('php://input');
+    file_put_contents('data/put_data.txt', $data);
+    echo '[PUT] Data written to file';
+} elseif ($method === 'DELETE') {
+    echo '[DELETE] delete called';
+} else {
+    // Handle invalid request
+    http_response_code(404);
+    echo 'Invalid request';
 }
+
 ?>
