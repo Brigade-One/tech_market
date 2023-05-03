acca71164f57ef73a9f9a3254554983e3fdfb347
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2023-05-03 13:00:17 +0300

    created header, added to Home, added assets

--- client/views/index.php
+++ client/views/index.php
@@ -1,7 +1,29 @@
-<?php
-echo '<h1>TECHMARKET HOME</h1>';
-echo '<br>';
-echo '<button onclick="location.href = \'http://techmarket/client/views/sign_in.php\';" >ВХІД</button>';
-echo '<br><br>';
-echo '<button onclick="location.href = \'http://techmarket/client/views/sign_up.php\';" >РЕЄСТРАЦІЯ</button>';
-?>
+<html>
+
+<head>
+    <title>TECHMARKET HOME</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
+    <div id="header">
+
+    </div>
+
+    <body>
+        <?php
+        echo '<h1>TECHMARKET HOME</h1>';
+        echo '<br>';
+        echo '<button onclick="location.href = \'http://techmarket/client/views/sign_in.php\';" >ВХІД</button>';
+        echo '<br><br>';
+        echo '<button onclick="location.href = \'http://techmarket/client/views/sign_up.php\';" >РЕЄСТРАЦІЯ</button>';
+        ?>
+    </body>
+
+</body>
+
+<script>
+    $("#header").load("widgets/header.html");
+</script>
+
+</html>
