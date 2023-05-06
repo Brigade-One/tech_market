f6bfeb2370582a31c438d19b8fa6c1b6901e7e62
Vitalii Petrun <87064627+vitalii-petrun@users.noreply.github.com>
2023-05-06 10:01:44 +0300

    problem fixed

--- client/src/js/sign_in.js
+++ client/src/js/sign_in.js
@@ -2,16 +2,14 @@ import { UserView } from "../../views/userView.js";
 import { User } from "../../models/user.js";
 import { UserController } from "../../controllers/userController.js";
 
-const form = document.querySelector('form');
+const form = document.querySelector('#sign-form');
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-
     // Get the form data
     const formData = new FormData(form);
     const user = new User('', formData.get('email'), formData.get('password'));
     const userView = new UserView();
     const userController = new UserController(user, userView);
-
     userController.handleSignIn();
 });
